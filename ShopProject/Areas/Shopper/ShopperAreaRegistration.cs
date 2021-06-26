cffad51235e3756eba69a7a3b45f754618d776c8
Phước <nguyenphuock18@gmail.com>
2021-06-26 17:29:37 +0700

    Add project files.

---
+++ ShopProject/Areas/Shopper/ShopperAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace ShopProject.Areas.Shopper
+{
+    public class ShopperAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Shopper";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Shopper_default",
+                "a/{controller}/{action}/{id}",
+                /*"Administrator/{controller}/{action}/{id}",*/
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
